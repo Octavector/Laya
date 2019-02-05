3251e5666c1f91571bfc46b0edc5987e29387a4c
Octavector <wehtam2@googlemail.com>
2019-02-05 19:50:01 +0000

    Using data attributes instead of classes

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-(function(){
+(function () {
     const CARD_LIST = [];
     const STAGE = document.querySelector('.stage');
     const CARDS = document.querySelector('.cards');
-    CARDS.addEventListener('click',cardsClick);
+    CARDS.addEventListener('click', cardsClick);
 
-    function cardsClick(e){
-        if(e.target.className === 'card-actor'){
+    function cardsClick(e) {
+        if (e.target.className === 'card-actor') {
             console.log('actor click');
         }
 
         switch (e.target.dataset.prefab) {
             case 'prefab1':
-              createRow('prefab1');
-              break;
+                createRow('prefab1');
+                break;
 
             default:
-              console.log('no prefab detected');
-          }
+                console.log('no prefab detected');
+        }
 
 
 
     }
 
-    function createRow(e){
+    function createRow(e) {
         console.log('building row ' + e);
         //1. find the matching prefab name in the PREFABS object
         //2. within the HTML property find the parent and create the obejct
@@ -30,35 +30,34 @@
         //4. add to the parent and add to the DOM.
 
         for (var prop in PREFABS) {
-            if(prop === e){
+            if (prop === e) {
                 //create the parent
-                let parentEl = createEl({tag:'div', classname:PREFABS[e].html.parent + ' ' + e});
-                PREFABS[e].html.children.forEach((e)=>{
-                    createEl({tag:'div', classname:e, parent:parentEl})
+                let parentEl = createEl({ tag: 'div', classname: PREFABS[e].html.parent + ' ' + e });
+                PREFABS[e].html.blocks.forEach((e) => {
+                    createEl({ tag: 'div', block: e, parent: parentEl, classname:'el' })
                 })
                 STAGE.appendChild(parentEl);
             }
         }
     }
 
-    function createEl({tag,classname,parent} = {}){
+    function createEl({ tag, block, parent, classname } = {}) {
         let e = document.createElement(tag);
-        e.setAttribute('class', classname);
-        if(parent){
-            parent.appendChild(e);
-        }
+        if (classname) { e.setAttribute('class', classname); }
+        if (block) { e.dataset.block = block; }
+        if (parent) { parent.appendChild(e); }
         return e;
     }
 
     // PREFABS
     // contains the instructional data for building the DOM elements the right way
     const PREFABS = {
-        prefab1:{
-            html:{
-                parent:'row',
-                children:['el pf1_el1','el pf1_el2','el pf1_el3','el pf1_el4']
+        prefab1: {
+            html: {
+                parent: 'row',
+                blocks: ['pf1_el1', 'pf1_el2', 'pf1_el3', 'pf1_el4']
             }
-            
+
 
         }
     }
